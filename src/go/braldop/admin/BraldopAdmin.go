a57a62507ffc065ef51a75f5ed6977f3356e6212
Canop <cano.petrole@gmail.com>
2011-12-01 19:43:23 +0100

    Version 2.1
    Gestion des peupliers balisés.
    Notification lorsque la version du client est plus ancienne que celle du serveur.
    Vérification de la version lors du passage sur la page web braldop.
    Commande bradmin (placeholder), permettant actuellement l'export de la palette
     des environnements pour le javascript.

---
+++ src/go/braldop/admin/BraldopAdmin.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"braldop/bra"
+	"flag"
+	"fmt"
+	"os"
+)
+
+
+
+
+func main() {
+
+	exportePalette := flag.Bool("palette", false, "si oui alors la palette des environnements est exportée (exemple : \"bradmin -palette\")")
+	flag.Parse()
+
+	if *exportePalette {
+		bra.ExportePalettePng(os.Stdout)
+	} else {
+		fmt.Fprintln(os.Stderr, "Usage :")
+		flag.PrintDefaults()
+	}
+}
